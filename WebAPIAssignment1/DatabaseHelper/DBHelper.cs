4928fe2aac70775f673ee0b7a4cf984d41cf70f9
Gail Peter Goncalves <gonc6532@outlook.com>
2019-01-13 16:19:25 -0800

    Created code first database. Set initial migration and updated the database

---
+++ WebAPIAssignment1/DatabaseHelper/DBHelper.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAssignment1.DatabaseHelper
+{
+    public class AccountType
+    {
+        [Key]
+        public int ID { get; set; }
+        public string accountDescription { get; set; }
+
+        // Navigation properties.
+        // Child.        
+        public virtual ICollection<ClientAccount>
+            ClientAccounts
+        { get; set; }
+    }
+
+    public class ClientProfile
+    {
+        [Key]
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        // Navigation properties.
+        // Child.
+        public virtual ICollection<ClientAccount>
+            ClientAccounts
+        { get; set; }
+    }
+
+    public class ClientAccount
+    {
+        [Key, Column(Order = 0)]
+        public int AccountID { get; set; }
+        [Key, Column(Order = 1)]
+        public int ClientID { get; set; }
+        public decimal Balance { get; set; }
+
+        // Navigation properties.
+        // Parents.
+        public virtual AccountType AccountType { get; set; }
+        public virtual ClientProfile ClientProfile { get; set; }
+    }
+
+    public class BankDbContext : DbContext
+    {
+        public BankDbContext(DbContextOptions<BankDbContext> options)
+            : base(options) { }
+
+        // Define entity collections.
+        public DbSet<AccountType> AccountTypes { get; set; }
+        public DbSet<ClientProfile> AccountProfiles { get; set; }
+        public DbSet<ClientAccount> ClientAccounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //---------------------------------------------------------------
+            // Define composite primary keys.
+            modelBuilder.Entity<ClientAccount>()
+                .HasKey(ps => new { ps.AccountID, ps.ClientID });
+
+            //---------------------------------------------------------------
+            // Define foreign keys here. Do not use foreign key annotations.
+            modelBuilder.Entity<ClientAccount>()
+                .HasOne(p => p.AccountType) // Parent
+                .WithMany(p => p.ClientAccounts) // Child
+                .HasForeignKey(fk => new { fk.AccountID })
+                .OnDelete(DeleteBehavior.Restrict); // Prevent cascade delete
+
+            modelBuilder.Entity<ClientAccount>()
+                .HasOne(p => p.ClientProfile) // Parent
+                .WithMany(p => p.ClientAccounts) // Child
+                .HasForeignKey(fk => new { fk.ClientID })
+                .OnDelete(DeleteBehavior.Restrict); // Prevent cascade delete
+
+            Seed(modelBuilder);
+        }
+
+        void Seed(ModelBuilder builder)
+        {
+            // Seed parents first and then children since child FK's point to parents.
+            builder.Entity<AccountType>().HasData(
+                new { ID = 1, Description = "Checking" },
+                new { ID = 2, Description = "Savings" }
+            );
+            builder.Entity<ClientProfile>().HasData(
+                new { ID = 1, LastName = "Jones", FirstName = "Barb" },
+                new { ID = 2, LastName = "Applewood", FirstName = "Bob" }
+            );
+            builder.Entity<ClientAccount>().HasData(
+                new { AccountID = 1, ClientID = 2, Balance = 101.51m },
+                new { AccountID = 2, ClientID = 1, Balance = 455.53m }
+            );
+
+        }
+    }
+}
---
+++ WebAPIAssignment1/Migrations/20190114001757_InitialCreate.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPIAssignment1.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountProfiles",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LastName = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountProfiles", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountTypes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    accountDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountTypes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientAccounts",
+                columns: table => new
+                {
+                    AccountID = table.Column<int>(nullable: false),
+                    ClientID = table.Column<int>(nullable: false),
+                    Balance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientAccounts", x => new { x.AccountID, x.ClientID });
+                    table.ForeignKey(
+                        name: "FK_ClientAccounts_AccountTypes_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "AccountTypes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ClientAccounts_AccountProfiles_ClientID",
+                        column: x => x.ClientID,
+                        principalTable: "AccountProfiles",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AccountProfiles",
+                columns: new[] { "ID", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "Barb", "Jones" },
+                    { 2, "Bob", "Applewood" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AccountTypes",
+                columns: new[] { "ID", "accountDescription" },
+                values: new object[,]
+                {
+                    { 1, null },
+                    { 2, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ClientAccounts",
+                columns: new[] { "AccountID", "ClientID", "Balance" },
+                values: new object[] { 2, 1, 455.53m });
+
+            migrationBuilder.InsertData(
+                table: "ClientAccounts",
+                columns: new[] { "AccountID", "ClientID", "Balance" },
+                values: new object[] { 1, 2, 101.51m });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientAccounts_ClientID",
+                table: "ClientAccounts",
+                column: "ClientID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientAccounts");
+
+            migrationBuilder.DropTable(
+                name: "AccountTypes");
+
+            migrationBuilder.DropTable(
+                name: "AccountProfiles");
+        }
+    }
+}
